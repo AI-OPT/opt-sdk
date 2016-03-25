0d7345774c9214ba4a0129f2ea97008b73773404
gucl <gucl@asiainfo.com>
2016-03-25 15:47:38 +0800

    mail test

--- src/test/java/com/ai/opt/sdk/test/paas/mail/MailTest.java
+++ src/test/java/com/ai/opt/sdk/test/paas/mail/MailTest.java
@@ -4,20 +4,19 @@ import org.junit.Test;
 
 import com.ai.opt.sdk.mail.EmailFactory;
 import com.ai.opt.sdk.mail.EmailTemplateUtil;
-import com.ai.opt.sdk.util.DateUtil;
 
 public class MailTest {
 
-	public static final String UPGRADE_NOTIFY_PUBLIC = "email/template/upgrade-notify-public.xml";
+	public static final String BIND_EMAIL = "email/template/uac-register-binemail.xml";
 
 	@Test
-	public void testSend() {
+	public void testSendBindEmail() {
 		String[] tomails = new String[] { "gucl@asiainfo.com" };
 		String[] ccmails = new String[] { "guchuanlong@126.com" };
 		String subject = "亚信云计费邮件测试";
-		String[] data = new String[] { "EmailFactory亚信云计费邮件测试亚信云计费邮件测试亚信云计费邮件测试"+DateUtil.getCurrentTime(), "EmailFactoryasdfdsafaf1232434"+DateUtil.getCurrentTime() };
-		String htmlcontext = EmailTemplateUtil.buildHtmlTextFromTemplate(UPGRADE_NOTIFY_PUBLIC, data);
-
+		String[] data = new String[] { "云計費nickname", "587434" ,"30"};
+		String htmlcontext = EmailTemplateUtil.buildHtmlTextFromTemplate(BIND_EMAIL, data);
+		
 		try {
 			EmailFactory.SendEmail(tomails, ccmails, subject, htmlcontext);
 		} catch (Exception e) {
