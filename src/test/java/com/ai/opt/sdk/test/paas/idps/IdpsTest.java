b840dc61bb0affbe4a622141b4c198bff5568861
gucl <gucl@asiainfo.com>
2016-05-25 12:45:37 +0800

    IDPS 测试类调优

--- src/test/java/com/ai/opt/sdk/test/paas/idps/IdpsTest.java
+++ src/test/java/com/ai/opt/sdk/test/paas/idps/IdpsTest.java
@@ -25,19 +25,19 @@ public class IdpsTest {
 		//应用场景
 		String idpsns="slp-mall-web-idps";
 		//获取imageClient
-		IImageClient im = IDPSClientFactory.getImageClient(idpsns);
+		IImageClient client = IDPSClientFactory.getImageClient(idpsns);
 		
 		//待上传的图片路径
 		String filepath="E:\\qie.jpg";
 		//将路径转换为byte[]
 		byte[] buff=ImageByteUtil.image2byte(filepath);
 		//上传图片，获取上传后的ID
-		String idpsId=im.upLoadImage(buff, "qie.jpg");
+		String idpsId=client.upLoadImage(buff, "qie.jpg");
 		System.out.println("idpsId="+idpsId);
 		//获取上传图片的URL
-		System.out.println(im.getImageUrl(idpsId, ".jpg"));
+		System.out.println(client.getImageUrl(idpsId, ".jpg"));
 		//获取上传图片指定尺寸的URL
-		System.out.println(im.getImageUrl(idpsId, ".png","100x80"));
+		System.out.println(client.getImageUrl(idpsId, ".png","100x80"));
 		
 
 	}
