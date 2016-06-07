ea76df873b5ecd13a4f96c76d6eb91542d590d18
myto2006 <myto2006@126.com>
2016-06-07 14:54:23 +0800

    初始化优化

--- src/main/java/com/ai/opt/sdk/components/base/ComponentConfigLoader.java
+++ src/main/java/com/ai/opt/sdk/components/base/ComponentConfigLoader.java
@@ -34,7 +34,7 @@ public final class ComponentConfigLoader {
      * @author zhangchao
      */
     public static ComponentConfigLoader getInstance() {
-        if (INSTANCE == null && prop==null) {
+        if (INSTANCE == null || prop==null) {
             // 多线程并发获取实例时候，避免等线程锁造成性能低下，因此在创建实例时候进行同步处理
             synchronized (ComponentConfigLoader.class) {
                 if (INSTANCE == null) {
@@ -47,7 +47,7 @@ public final class ComponentConfigLoader {
 
     }
     public static ComponentConfigLoader loadPaaSConf(Properties p) {
-    	  if (INSTANCE == null && prop==null) {
+    	  if (INSTANCE == null || prop==null) {
               // 多线程并发获取实例时候，避免等线程锁造成性能低下，因此在创建实例时候进行同步处理
               synchronized (ComponentConfigLoader.class) {
                   if (INSTANCE == null) {
