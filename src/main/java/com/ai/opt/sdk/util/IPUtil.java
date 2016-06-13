e1845a42927f1d13c098e37eff836f353e3efd49
gucl <gucl@asiainfo.com>
2016-06-13 16:40:42 +0800

    IPUtil

--- src/main/java/com/ai/opt/sdk/util/IPUtil.java
+++ src/main/java/com/ai/opt/sdk/util/IPUtil.java
@@ -1,7 +1,10 @@
 package com.ai.opt.sdk.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class IPUtil {
-	   // 将127.0.0.1形式的IP地址转换成十进制整数，这里没有进行任何错误处理
+	// 将127.0.0.1形式的IP地址转换成十进制整数，这里没有进行任何错误处理
 		public static long ipToLong(String strIp) {
 			long[] ip = new long[4];
 			// 先找到IP地址字符串中.的位置
@@ -32,9 +35,72 @@ public class IPUtil {
 			sb.append(String.valueOf((longIp & 0x000000FF)));
 			return sb.toString();
 		}
-		
-		/*public static void main(String[] args) {
-			System.out.println(longToIP(3689901706L));
-			System.out.println(ipToLong("219.239.110.138"));			
-		}*/
+
+		/**
+		 * 判断ipAddress是否为IP
+		 * 
+		 * @param ipAddress
+		 * @return
+		 * @author gucl
+		 */
+		public static boolean isIP(String ipAddress) {
+			if (ipAddress.length() < 7 || ipAddress.length() > 15 || "".equals(ipAddress)) {
+				return false;
+			}
+			/**
+			 * 判断IP格式和范围
+			 */
+			String rexp = "([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}";
+
+			Pattern pat = Pattern.compile(rexp);
+
+			Matcher mat = pat.matcher(ipAddress);
+
+			return mat.find();
+		}
+		/**
+		 * 判断ipAddress是否为内网IP
+		 * @param ipAddress
+		 * @return
+		 * @author gucl
+		 */
+		public static boolean isInnerIP(String ipAddress) {
+			boolean isInnerIp = false;
+			boolean isIPFlag = isIP(ipAddress);
+			if (isIPFlag) {
+				long ipNum = getIpNum(ipAddress);
+				/**
+				 * 私有IP：A类 10.0.0.0-10.255.255.255 B类 172.16.0.0-172.31.255.255 C类
+				 * 192.168.0.0-192.168.255.255 当然，还有127这个网段是环回地址
+				 **/
+				long aBegin = getIpNum("10.0.0.0");
+				long aEnd = getIpNum("10.255.255.255");
+				long bBegin = getIpNum("172.16.0.0");
+				long bEnd = getIpNum("172.31.255.255");
+				long cBegin = getIpNum("192.168.0.0");
+				long cEnd = getIpNum("192.168.255.255");
+				isInnerIp = isInner(ipNum, aBegin, aEnd) || isInner(ipNum, bBegin, bEnd) || isInner(ipNum, cBegin, cEnd)
+						|| ipAddress.equals("127.0.0.1");
+			} else {
+				if ("localhost".equalsIgnoreCase(ipAddress)) {
+					isInnerIp = true;
+				}
+			}
+			return isInnerIp;
+		}
+
+		private static long getIpNum(String ipAddress) {
+			String[] ip = ipAddress.split("\\.");
+			long a = Integer.parseInt(ip[0]);
+			long b = Integer.parseInt(ip[1]);
+			long c = Integer.parseInt(ip[2]);
+			long d = Integer.parseInt(ip[3]);
+
+			long ipNum = a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d;
+			return ipNum;
+		}
+
+		private static boolean isInner(long userIp, long begin, long end) {
+			return (userIp >= begin) && (userIp <= end);
+		}
 }
