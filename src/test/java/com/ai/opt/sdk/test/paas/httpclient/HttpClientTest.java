bd8f5364e2f29de8abd42ee2534e3ce5a2fd54b4
gucl <gucl@asiainfo.com>
2016-06-20 14:54:30 +0800

    httpclient

--- src/test/java/com/ai/opt/sdk/test/paas/httpclient/HttpClientTest.java
+++ src/test/java/com/ai/opt/sdk/test/paas/httpclient/HttpClientTest.java
@@ -12,22 +12,30 @@ public class HttpClientTest {
 
 	
 	@Test
-	public void testSendPostDemo(){
-		String url="http://10.1.245.9:20882/slp-order/demo/hello";
-		Map<String,String> parameters=new HashMap<String,String>();
-		parameters.put("name", "worlddemo");
-		String data=JSON.toJSONString(parameters);
+	public void testSendPostHello(){
+		String url="http://10.1.245.9:10887/slp-order/demo/hello";
 		String json=HttpClientUtil.sendPost(url, "worlddemo");
 		System.out.println("testSendPostClient="+json);
 	}
 	@Test
-	public void testSendPostOrder(){
-		String url="http://10.1.245.9:20882/slp-order/demo/hello";
+	public void testSendPostHelloParam(){
+		String url="http://10.1.245.9:10887/slp-order/demo/helloParam";
 		Map<String,String> parameters=new HashMap<String,String>();
-		parameters.put("name", "worlddemo");
+		parameters.put("name", "worldparam");
 		String data=JSON.toJSONString(parameters);
-		String json=HttpClientUtil.sendPost(url, "worlddemo");
+		String json=HttpClientUtil.sendPost(url, data);
 		System.out.println("testSendPostClient="+json);
 	}
+	 @Test
+    public void orderListTestByRest() {
+	   Map<String,String> params=new HashMap<String,String>();
+	   params.put("orderId", "78436478");
+	   params.put("tenantId", "SLP");
+        String url="http://10.1.245.9:10887/slp-order/orderlist/queryOrder";
+        String param=JSON.toJSONString(params);
+        String result=HttpClientUtil.sendPost(url, param);
+        System.out.println("param="+JSON.toJSONString(params));
+        System.out.println("result="+result);
+    }
 	
 }
