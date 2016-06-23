28a50278cc7bc80edf26aebd632190ff4bcadd8f
gucl <gucl@asiainfo.com>
2016-06-23 21:39:40 +0800

    dts优化   测试类

--- src/main/java/com/ai/opt/sdk/dts/demo/TestTask.java
+++ src/main/java/com/ai/opt/sdk/dts/demo/TestTask.java
@@ -5,16 +5,19 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.PersistJobDataAfterExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ai.opt.sdk.dts.base.ITask;
 
 @DisallowConcurrentExecution
 @PersistJobDataAfterExecution
 public class TestTask implements ITask {
-
+	private static final Logger LOG = LoggerFactory.getLogger(TestTask.class);
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         System.out.println("开始执行任务了");
+        LOG.error("开始执行任务了");
 
     }
 
--- src/main/java/com/ai/opt/sdk/dts/main/DTSMain.java
+++ src/main/java/com/ai/opt/sdk/dts/main/DTSMain.java
@@ -4,6 +4,7 @@ package com.ai.opt.sdk.dts.main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ai.opt.sdk.dts.constants.DTSConstants;
 import com.ai.opt.sdk.dts.factory.DTSSchedulerFactory;
 import com.ai.paas.ipaas.util.StringUtil;
 
@@ -16,18 +17,18 @@ public class DTSMain {
     }
 
     public static void main(String[] args) {
-        LOG.debug("开始启动dts调度任务..");
-        String systemName = System.getProperty("opt.system.name");
-        if (StringUtil.isBlank(systemName)) {
-            LOG.error("无法从启动参数获取系统名称，请检查参数[-Dopt.system.name=XX]");
+        LOG.error("开始启动dts调度任务..");
+        String schedulerName = System.getProperty(DTSConstants.OPT_SCHEDULER_NAME);
+        if (StringUtil.isBlank(schedulerName)) {
+            LOG.error("无法从启动参数获取系统名称，请检查参数[-Dopt.scheduler.name=XX]");
             System.exit(-1);
         }
 
         try {
-            DTSSchedulerFactory.start(systemName);
-            LOG.error("系统[" + systemName + "]的DTS实例启动没有报错，请耐心观察后续日志是否成功");
+            DTSSchedulerFactory.start(schedulerName);
+            LOG.error("系统[" + schedulerName + "]的DTS实例启动没有报错，请耐心观察后续日志是否成功");
         } catch (Exception ex) {
-            LOG.error("系统[" + systemName + "]的DTS实例启动失败", ex);
+            LOG.error("系统[" + schedulerName + "]的DTS实例启动失败", ex);
             System.exit(-1);
         }
         while (true) {
