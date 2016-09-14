b32f2a6fba71c27d86a9672dea57d1e9065b11de
myto2006 <myto2006@126.com>
2016-09-14 15:10:58 +0800

    httpclientutil增加编码控制

--- src/main/java/com/ai/opt/sdk/dubbo/util/HttpClientUtil.java
+++ src/main/java/com/ai/opt/sdk/dubbo/util/HttpClientUtil.java
@@ -39,6 +39,7 @@ public class HttpClientUtil {
     public static String sendPost(String url, String param, Map<String, String> header) throws IOException, URISyntaxException {
     	logger.info("restful request url:"+url);
     	logger.info("restful request param:"+param);
+    	String charset = "utf-8";
     	DubboRestResponse resp=new DubboRestResponse();
     	StringBuffer buffer = new StringBuffer();
         CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -46,6 +47,9 @@ public class HttpClientUtil {
         if(header!=null){
         	for (Map.Entry<String, String> entry : header.entrySet()) {
         		httpPost.setHeader(entry.getKey(), entry.getValue());
+        		if("charset".equals(entry.getKey())){
+                    charset = entry.getValue();
+                }
         	}        	
         }
         StringEntity dataEntity = new StringEntity(param, ContentType.APPLICATION_JSON);
@@ -56,7 +60,7 @@ public class HttpClientUtil {
             if (response.getStatusLine().getStatusCode() == 200) {
                 HttpEntity entity = response.getEntity();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        entity.getContent()));
+                        entity.getContent(), charset));
                 String tempStr;
                 while ((tempStr = reader.readLine()) != null)
                     buffer.append(tempStr);
